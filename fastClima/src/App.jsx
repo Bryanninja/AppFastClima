bf5d2d10d4b8c52d1886f4ee55c83c491a9d8b8f
Álex Bryan <bryannascimentopl800@gmail.com>
2025-11-27 15:21:44 -0300

    feat: add animation on City component

--- fastClima/src/App.jsx
+++ fastClima/src/App.jsx
@@ -1,28 +1,41 @@
-import './App.css'
-import BackgroundVideo from './BackgroundVideo';
-import { useState } from 'react';
-import City from './City';
-import Search from './Search';
+import "./App.css";
+import BackgroundVideo from "./BackgroundVideo";
+import { useState } from "react";
+import City from "./City";
+import Search from "./Search";
 
 function App() {
-  const[city, setCity] = useState(null)
-  const [erro, setErro] = useState(null)
-  const [sugestoes, setSugestoes] = useState([])
+  const [city, setCity] = useState(null);
+  const [erro, setErro] = useState(null);
+  const [sugestoes, setSugestoes] = useState([]);
   return (
- <div className="App">
-
-      <BackgroundVideo/>
-      <Search setCity={setCity} setErro={setErro} setSugestoes={setSugestoes} sugestoes={sugestoes} />
+    <div className="App">
+      <BackgroundVideo />
+      <Search
+        setCity={setCity}
+        setErro={setErro}
+        setSugestoes={setSugestoes}
+        sugestoes={sugestoes}
+      />
 
       {erro ? (
         <div className="erroMsg center">❌ {erro}</div>
       ) : city ? (
         <City data={city} />
       ) : (
-        <div className="infoMsg center">🔍 Pesquise uma cidade para ver o clima!</div>
+        <div className="infoMsg center">
+          <h3>🔍 Pesquise uma cidade para ver o clima!</h3>
+        </div>
       )}
+
+      <div className="footer">
+        <p>
+          Desenvolvido por Álex Bryan. Focado em Web Design e Integração de API.
+        </p>
+        <p>E-mail: bryanncode@gmail.com @bryanndesign @alexbryannt </p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
